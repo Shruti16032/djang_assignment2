81641a2226753609ba92079140efc60cb4b1e9ec
Shruti Todkar <80196143+Shruti16032@users.noreply.github.com>
2025-04-21 22:49:02 +0530

    Add files via upload

---
+++ task_manager/tasks/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .forms import *
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'create_task.html', {'form': form})
+
+def task_list(request):
+    tasks = Task.objects.all().order_by('due_date')
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+def task_detail(request, pk):
+    task = Task.objects.get(pk=pk)
+    return render(request, 'task_detail.html', {'task': task})
+
+def update_task(request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'update_task.html', {'form': form})
+
+def delete_task(request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'delete_task.html', {'task': task})    
